e10731271e2c4450ce0ecd124074c55b1be65db3
sjones97 <sjones97@email.unc.edu>
2019-11-04 20:01:03 -0500

    novice passing commit

--- src/a6/ProximityIterator.java
+++ src/a6/ProximityIterator.java
@@ -4,41 +4,45 @@ import java.util.*;
 
 public class ProximityIterator implements Iterator<Driver> {
 
-    private Iterable<Driver> driver_pool;
-    private Position client_position;
-    private int proximity_limit;
-    private Driver driver;
+    private Iterable<Driver> _driverPool;
+    private Position _clientPosition;
+    private int _proximityLimit;
+    private Driver _nextDriver;
+    private Iterator<Driver> _driverIterator;
 
     public ProximityIterator(Iterable<Driver> driver_pool, Position client_position, int proximity_limit) {
-        this.driver_pool = driver_pool;
-        this.client_position = client_position;
-        this.proximity_limit = proximity_limit;
-        this.driver = null;
+        if (driver_pool == null || client_position == null || proximity_limit < 0) {
+            throw new IllegalArgumentException();
+        }
+        this._driverPool = driver_pool;
+        this._clientPosition = client_position;
+        this._proximityLimit = proximity_limit;
+        this._nextDriver = null;
+        this._driverIterator = driver_pool.iterator();
     }
 
 
     @Override
     public boolean hasNext() {
-        this.next();
-        if (driver == null) {
-            return false;
-        } else {
+        if (_nextDriver != null && _nextDriver.getVehicle().getPosition().getManhattanDistanceTo(_clientPosition) <= _proximityLimit) {
             return true;
         }
+        while (_driverIterator.hasNext()) {
+            _nextDriver = _driverIterator.next();
+            if (_nextDriver.getVehicle().getPosition().getManhattanDistanceTo(_clientPosition) <= _proximityLimit) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public Driver next() {
-//        if (!hasNext()) {
-//            throw new NoSuchElementException();
-//        }
-        for (Driver tempDriver : driver_pool) {
-            if (tempDriver.getVehicle().getPosition().getManhattanDistanceTo(client_position) <= proximity_limit) {
-                driver = tempDriver;
-                tempDriver = null;
-                return driver;
-            }
+        if (!this.hasNext()) {
+            throw new NoSuchElementException();
         }
-        return null;
+        Driver tempDriver = _nextDriver;
+        _nextDriver = null;
+        return tempDriver;
     }
 }
