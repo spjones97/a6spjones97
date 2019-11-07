5f5cb05b5656a9d91d9161926777c8c06cba4242
sjones97 <sjones97@email.unc.edu>
2019-11-07 14:02:06 -0500

    example commit

--- src/a6/SnakeOrderAcrossPoolsIterator.java
+++ src/a6/SnakeOrderAcrossPoolsIterator.java
@@ -3,40 +3,72 @@ package a6;
 import java.util.*;
 
 public class SnakeOrderAcrossPoolsIterator implements Iterator<Driver> {
-    private List<Iterable<Driver>> driver_pools;
-    private Iterable<Driver> driverPool;
-    private Driver driver;
-    private int index;
+    private List<Iterable<Driver>> _listOfPools;
+    private List<Iterator<Driver>> _listOfIterators;
+    private Driver _nextDriver;
+    private int index = 1;
+    private boolean forwards = true;
+    private boolean backwards = false;
 
     public SnakeOrderAcrossPoolsIterator(List<Iterable<Driver>> driver_pools) {
-        this.driver_pools = driver_pools;
-        this.driver_pools = null;
-        this.driver = null;
+        if (driver_pools == null) {
+            throw new IllegalArgumentException("Input value is null");
+        }
+        this._listOfPools = driver_pools;
+        this._listOfIterators = new ArrayList<Iterator<Driver>>();
+        for (Iterable<Driver> i : _listOfPools) {
+            _listOfIterators.add(i.iterator());
+        }
+        this._nextDriver = null;
     }
 
     @Override
     public boolean hasNext() {
-        if (driver == null) {
-            return false;
-        } else {
-            this.next();
-            if (driver == null) {
-                return false;
-            } else {
-                return true;
+        boolean start = false;
+        for (int i = 0; i < _listOfIterators.size(); i++) {
+            if (_listOfIterators.get(i).hasNext()) {
+                start = true;
             }
         }
+        while (start) {
+            if (forwards) {
+                for (int i = index; i < _listOfIterators.size(); i++) {
+                    if (_listOfIterators.get(i).hasNext()) {
+                        _nextDriver = _listOfIterators.get(i).next();
+                        return true;
+                    }
+                }
+            }
+            if (backwards) {
+                for (int i = index; i >= 0; i--) {
+                    if (_listOfIterators.get(i).hasNext()) {
+                        _nextDriver = _listOfIterators.get(i).next();
+                        return true;
+                    }
+                }
+            }
+            forwards = !forwards;
+            backwards = !backwards;
+            for (Iterable<Driver> i : _listOfPools) {
+                _listOfIterators.add(i.iterator());
+            }
+        }
+        return false;
     }
 
     @Override
     public Driver next() {
-        for (Iterable<Driver> tempPool : driver_pools) {
-            for (Driver tempDriver : tempPool) {
-                driver = tempDriver;
-                tempDriver = null;
-                return driver;
-            }
+        if (forwards) {
+            index++;
         }
-        return null;
+        if (backwards) {
+            index--;
+        }
+        if (!this.hasNext()) {
+            throw new NoSuchElementException();
+        }
+        Driver tempDriver = _nextDriver;
+        _nextDriver = null;
+        return tempDriver;
     }
 }
